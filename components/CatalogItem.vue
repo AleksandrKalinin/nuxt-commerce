9acbddc0e2b695106daf599df1195766a669270e
AleksandrKalinin <aleksandr_kalinin_1995@mail.ru>
2023-05-12 18:06:33 +0300

    refactor: Minor style fixes

--- components/CatalogItem.vue
+++ components/CatalogItem.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="p-5 border sky-blue-400 mb-5 max-w-[300px]">
     <div class="flew lg:w-60 lg:h-36 w-40 h-24">
-      <img class="w-fit h-full object-cover" src="/images/1.jpg" />
+      <img class="w-fit h-full mx-auto object-cover" src="/images/1.jpg" />
     </div>
     <div class="py-5">
-      <h2 class="text-xl flex justify-between items-center font-semibold py-2">
+      <h2 class="text-lg flex justify-between items-center font-semibold py-2">
         Canon EOS 6D <span class="">2400$</span>
       </h2>
       <p class="text-base mb-4">Lorem ipsum dolor sit amet</p>
       <button
-        class="text-lg px-8 mx-auto py-3 bg-sky-400 text-white rounded-none"
+        class="text-lg px-7 mx-auto py-2 bg-sky-400 text-white rounded-none"
       >
         Add to cart
       </button>
--- components/CatalogPanel.vue
+++ components/CatalogPanel.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="navigation w-100 flex justify-between items-center pb-5">
     <div class="navigation__sorting flex justify-between">
-      <div class="sorting-option mr-3 text-sm">По дате добавления</div>
-      <div class="sorting-option mr-3 text-sm">По цене</div>
+      <span class="sorting-option mr-3 text-sm cursor-pointer"
+        >По дате добавления</span
+      >
+      <span class="sorting-option mr-3 text-sm cursor-pointer">По цене</span>
     </div>
     <div class="navigation__search">
       <input type="text" placeholder="Search" class="py-2 px-4" />
