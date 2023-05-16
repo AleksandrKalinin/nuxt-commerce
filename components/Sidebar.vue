110dcc2eac819281abc92cadca41fdaa918dc3b7
AleksandrKalinin <aleksandr_kalinin_1995@mail.ru>
2023-05-16 19:24:10 +0300

    feat: Fixed filtering for catalog items, put all related logic into pinia store module

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <aside class="w-96 border sky-blue-400">
     <div
-      v-for="item in filteringOptions"
+      v-for="item in store.filteringOptions"
       class="filter-section border-b-1 sky-blue-400 p-5"
     >
       <h3 class="filter-section__title text-xl font-semibold mb-3">
@@ -14,7 +14,7 @@
               type="checkbox"
               class="mr-2 w-7 h-7"
               v-model="option.selected"
-              @change="selectItem(option.label, item.category)"
+              @change="store.selectItem(option.label, item.category)"
             />{{ option.label }}</label
           >
         </div>
@@ -27,130 +27,9 @@
 import { useCatalogStore } from "~/store/catalog";
 const store = useCatalogStore();
 
-const manufacturers = computed(() => {
-  const labels = [
-    ...new Set(store.catalogItems.map((item) => item.manufacturer)),
-  ].map((label) => {
-    const obj = {};
-    obj.label = label;
-    obj.selected = false;
-    return obj;
-  });
-  return labels;
+const catalogItems = computed(() => {
+  return store.sortedItems;
 });
-
-const types = computed(() => {
-  const labels = [...new Set(store.catalogItems.map((item) => item.type))].map(
-    (label) => {
-      const obj = {};
-      obj.label = label;
-      obj.selected = false;
-      return obj;
-    }
-  );
-  return labels;
-});
-
-const matrixTypes = computed(() => {
-  const labels = [
-    ...new Set(store.catalogItems.map((item) => item.matrix_type)),
-  ].map((label) => {
-    const obj = {};
-    obj.label = label;
-    obj.selected = false;
-    return obj;
-  });
-  return labels;
-});
-
-const matrixSizes = computed(() => {
-  const labels = [
-    ...new Set(store.catalogItems.map((item) => item.matrix_size)),
-  ].map((label) => {
-    const obj = {};
-    obj.label = label;
-    obj.selected = false;
-    return obj;
-  });
-  return labels;
-});
-
-const matrixPixels = computed(() => {
-  const labels = [
-    ...new Set(store.catalogItems.map((item) => item.pixels)),
-  ].map((label) => {
-    const obj = {};
-    obj.label = label;
-    obj.selected = false;
-    return obj;
-  });
-  return labels;
-});
-
-const filteringOptions = computed(() => {
-  return [
-    {
-      label: "Производитель",
-      value: manufacturers.value,
-      category: "manufacturer",
-    },
-    {
-      label: "Тип камеры",
-      value: types.value,
-      category: "type",
-    },
-    {
-      label: "Тип матрицы",
-      value: matrixTypes.value,
-      category: "matrix_type",
-    },
-    {
-      label: "Размер матрицы",
-      value: matrixSizes.value,
-      category: "matrix_size",
-    },
-    {
-      label: "Кол-в точек матрицы",
-      value: matrixPixels.value,
-      category: "pixels",
-    },
-  ];
-});
-
-/*
-const filteredItems = computed(() => {
-  console.log(selectedOptions);
-  return store.catalogItems.map((curItem) => {
-    for (const key in selectedOptions) {
-      const values = selectedOptions[key];
-      if (values.length) {
-        if (!values.includes(curItem[key])) {
-          return false;
-        }
-      }
-    }
-    return curItem;
-  });
-});
-*/
-
-const selectedOptions = {
-  manufacturer: [],
-  type: [],
-  matrix_type: [],
-  matrix_size: [],
-  pixels: [],
-};
-
-const selectItem = (value, category) => {
-  if (selectedOptions[category].includes(value)) {
-    selectedOptions[category] = selectedOptions[category].filter(
-      (item) => item !== value
-    );
-  } else {
-    selectedOptions[category].push(value);
-  }
-};
 </script>
 
 <style scoped></style>
--- store/catalog.ts
+++ store/catalog.ts
@@ -71,6 +71,129 @@ export const useCatalogStore = defineStore("catalog", () => {
     }
   });
 
+  const manufacturers = computed(() => {
+    const labels = [
+      ...new Set(catalogItems.value.map((item) => item.manufacturer)),
+    ].map((label) => {
+      const obj = {};
+      obj.label = label;
+      obj.selected = false;
+      return obj;
+    });
+    return labels;
+  });
+
+  const types = computed(() => {
+    const labels = [
+      ...new Set(catalogItems.value.map((item) => item.type)),
+    ].map((label) => {
+      const obj = {};
+      obj.label = label;
+      obj.selected = false;
+      return obj;
+    });
+    return labels;
+  });
+
+  const matrixTypes = computed(() => {
+    const labels = [
+      ...new Set(catalogItems.value.map((item) => item.matrix_type)),
+    ].map((label) => {
+      const obj = {};
+      obj.label = label;
+      obj.selected = false;
+      return obj;
+    });
+    return labels;
+  });
+
+  const matrixSizes = computed(() => {
+    const labels = [
+      ...new Set(catalogItems.value.map((item) => item.matrix_size)),
+    ].map((label) => {
+      const obj = {};
+      obj.label = label;
+      obj.selected = false;
+      return obj;
+    });
+    return labels;
+  });
+
+  const matrixPixels = computed(() => {
+    const labels = [
+      ...new Set(catalogItems.value.map((item) => item.pixels)),
+    ].map((label) => {
+      const obj = {};
+      obj.label = label;
+      obj.selected = false;
+      return obj;
+    });
+    return labels;
+  });
+
+  const filteringOptions = computed(() => {
+    return [
+      {
+        label: "Производитель",
+        value: manufacturers.value,
+        category: "manufacturer",
+      },
+      {
+        label: "Тип камеры",
+        value: types.value,
+        category: "type",
+      },
+      {
+        label: "Тип матрицы",
+        value: matrixTypes.value,
+        category: "matrix_type",
+      },
+      {
+        label: "Размер матрицы",
+        value: matrixSizes.value,
+        category: "matrix_size",
+      },
+      {
+        label: "Кол-в точек матрицы",
+        value: matrixPixels.value,
+        category: "pixels",
+      },
+    ];
+  });
+
+  const selectedOptions = reactive({
+    manufacturer: [],
+    type: [],
+    matrix_type: [],
+    matrix_size: [],
+    pixels: [],
+  });
+
+  const selectedItems = computed(() => {
+    return sortedItems.value.filter((curItem) => {
+      let flag = true;
+      for (const key in selectedOptions) {
+        const values = selectedOptions[key];
+        if (values.length) {
+          if (!values.includes(curItem[key])) {
+            flag = false;
+          }
+        }
+      }
+      if (flag) return curItem;
+    });
+  });
+
+  const selectItem = (value, category) => {
+    if (selectedOptions[category].includes(value)) {
+      selectedOptions[category] = selectedOptions[category].filter(
+        (item) => item !== value
+      );
+    } else {
+      selectedOptions[category].push(value);
+    }
+  };
+
   return {
     catalogItems,
     fetchCatalogItems,
@@ -80,5 +203,9 @@ export const useCatalogStore = defineStore("catalog", () => {
     searchValue,
     updateSort,
     sortedItems,
+    selectedOptions,
+    filteringOptions,
+    selectedItems,
+    selectItem,
   };
 });
