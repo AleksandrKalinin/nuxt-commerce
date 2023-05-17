ba411aa96481bf012e487fe6ed2fd378b6e93aec
AleksandrKalinin <aleksandr_kalinin_1995@mail.ru>
2023-05-17 09:12:48 +0300

    feat: Implemented basic pagination

--- components/Base/BasePagination.vue
+++ components/Base/BasePagination.vue
@@ -2,35 +2,53 @@
   <div class="pagination-container w-full flex justify-end mt-3">
     <div class="pagination flex cursor-pointer">
       <div
+        @click="firstPage"
         class="flex justify-center items-center border sky-blue-400 font-lg font-semibold px-3"
       >
-        Previous
+        First
       </div>
       <div
-        class="pagination-item pagination-item_active flex justify-center items-center border sky-blue-400 w-10 h-10 font-lg font-semibold"
-      >
-        1
-      </div>
-      <div
+        v-for="num in pageNumbers"
+        @click="setPage(num)"
+        :class="{ 'pagination-item_active': num === store.currentPage + 1 }"
         class="pagination-item flex justify-center items-center border sky-blue-400 w-10 h-10 font-lg font-semibold"
       >
-        2
+        {{ num }}
       </div>
       <div
-        class="pagination-item flex justify-center items-center border sky-blue-400 w-10 h-10 font-lg font-semibold"
-      >
-        3
-      </div>
-      <div
+        @click="lastPage"
         class="flex justify-center items-center border sky-blue-400 font-lg font-semibold px-3"
       >
-        Next
+        Last
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useCatalogStore } from "~/store/catalog";
+const store = useCatalogStore();
+const pageNumbers = computed(() => {
+  let numbers = [];
+  for (let i = 1; i <= Math.ceil(store.selectedItems.length / 12); i++) {
+    numbers.push(i);
+  }
+  return numbers;
+});
+
+const firstPage = () => {
+  store.currentPage = 0;
+};
+
+const lastPage = () => {
+  const maxLength = Math.ceil(store.selectedItems.length / 12);
+  store.currentPage = maxLength - 1;
+};
+
+const setPage = (num) => {
+  store.currentPage = num - 1;
+};
+</script>
 
 <style scoped>
 .pagination-item {
