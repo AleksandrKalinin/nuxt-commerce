00b559c6c830098041ff3b03791b486efd4a50d8
AleksandrKalinin <aleksandr_kalinin_1995@mail.ru>
2023-06-09 15:07:44 +0300

    fix: Changed Nuxt config setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,8 +7,8 @@ export default defineNuxtConfig({
     },
   },
   typescript: {
-    strict: true,
-    typeCheck: true,
+    strict: false,
+    typeCheck: false,
   },
   components: [
     "~/components",
